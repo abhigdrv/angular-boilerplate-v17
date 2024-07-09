0d127f8df14254d2907b13acc810a6842f90f7e5
Abhishek Vishwakarma <52538014+abhigdrv@users.noreply.github.com>
2024-07-09 18:53:08 +0530

    fix: minor fix

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'angular-boilerplate-v16'`, () => {
+  it(`should have as title 'angular-boilerplate-v17'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('angular-boilerplate-v16');
+    expect(app.title).toEqual('angular-boilerplate-v17');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('angular-boilerplate-v16 app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('angular-boilerplate-v17 app is running!');
   });
 });
